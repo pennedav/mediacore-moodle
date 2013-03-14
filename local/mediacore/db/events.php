9cba817fe7490dd634092dad9ea97b26b12e8927
Derek Harnanansingh <derek@mediacore.com>
2013-03-14 10:54:12 -0700

    Update db/events.php doc strings

--- local/mediacore/db/events.php
+++ local/mediacore/db/events.php
@@ -6,27 +6,13 @@
  *    / /  / / /___/ /_/ /_/ // ___ / /___/ /_/ / _, _/ /___
  *   /_/  /_/_____/_____//___/_/  |_\____/\____/_/ |_/_____/
  *
- * Moodle is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Moodle is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- * MediaCore's local plugin event handler
+ * MediaCore's local plugin
  *
  * @package    local
  * @subpackage mediacore
  * @copyright  2012 MediaCore Technologies
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
  */
 $handlers = array(
 	'course_deleted' => array(
