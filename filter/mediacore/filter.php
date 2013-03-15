d0d7fb0a293d8f7bfb6d2d794c0a43efa0e162d2
Derek Harnanansingh <derek@mediacore.com>
2013-03-15 00:47:26 -0700

    Updat the filter
    * Do url pattern matching on the hostname
    * Make the DomDocument saveHTML function compatible with older version
        of PHP 5
    * Simplify the fetch_embed_code helper function

--- filter/mediacore/filter.php
+++ filter/mediacore/filter.php
@@ -25,34 +25,33 @@ require_once($CFG->libdir . '/filelib.php');
  */
 class filter_mediacore extends moodle_text_filter {
 
-    private $_mediacore_url;
+    private $_baseurl;
     private $_mcore_media;
 
     public function __construct($context, array $localconfig) {
-        $this->_mediacore_url = local_mediacore_fetch_lti_url();
-        $this->_mcore_media = new mediacore_media($this->_mediacore_url);
+        $this->_baseurl = local_mediacore_fetch_lti_baseurl();
+        $this->_hostname = parse_url($this->_baseurl, PHP_URL_HOST);
+        $this->_mcore_media = new mediacore_media($this->_baseurl);
         parent::__construct($context, $localconfig);
     }
 
     public function filter($html, array $options = array()) {
-
-        if (!is_string($html) || empty($html) || stripos($html, '</a>' === FALSE) ||
-            strpos($html, $this->_mediacore_url) === FALSE) { //performance hack
+        if (empty($html) || !is_string($html) || stripos($html, '</a>' === FALSE) ||
+            strpos($html, $this->_hostname) === FALSE) { //performance hack
                 return $html;
         }
-
         $dom = new DomDocument();
         $dom->loadHtml(mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8"));
         $xpath = new DOMXPath($dom);
         foreach($xpath->query('//a') as $node) {
             $href = $node->attributes->getNamedItem('href')->nodeValue;
-            if (stripos($href, $this->_mediacore_url) !== FALSE) {
+            if (stripos($href, $this->_hostname) !== FALSE) {
                 $new_node  = $dom->createDocumentFragment();
                 $new_node->appendXML($this->_fetch_embed_code($href));
                 $node->parentNode->replaceChild($new_node, $node);
             }
         }
-        return $dom->saveHTML($dom->documentElement);
+        return $dom->saveHTML();
     }
 
     /**
@@ -60,7 +59,7 @@ class filter_mediacore extends moodle_text_filter {
      */
     private function _fetch_embed_code($href) {
         global $COURSE;
-        $type_id = null;
+        $type_id = NULL;
         $msg = get_string('filter_no_video_found', 'filter_mediacore');
 
         // Parse the link so we can get to the slug and type_id (if applicable).
@@ -69,28 +68,27 @@ class filter_mediacore extends moodle_text_filter {
         $slug = end($path_arr);
         foreach (explode('&', $uri_components['query']) as $kv) {
             $arr = explode('=', $kv);
-            if (isset($arr[0]) && $arr[0] == 'type_id') {
+            if (isset($arr[0], $arr[1]) && $arr[0] == 'type_id') {
                 $type_id = (int)$arr[1];
                 break;
             }
         }
 
-        if ($type_id >= 0) {
-            if ($type_id > 0) {
-                $type_ids = local_mediacore_fetch_lti_tool_ids_by_course_id($COURSE->id);
-                if (empty($type_ids)) {
-                    $msg = get_string('filter_error_no_type_id', 'filter_mediacore');
-                    return $this->_get_embed_error_html($msg);
-                } elseif (!in_array($type_id, explode(',', $type_ids->value))) {
-                    $msg = get_string('filter_no_type_mapping_error', 'filter_mediacore');
-                    return $this->_get_embed_error_html($msg);
-                }
+        if (isset($type_id) && $type_id > 0) {
+            $type_ids = local_mediacore_fetch_lti_tool_ids_by_course_id($COURSE->id);
+            if (empty($type_ids)) {
+                $msg = get_string('filter_error_no_type_id', 'filter_mediacore');
+                return $this->_get_embed_error_html($msg);
+            } elseif (!in_array($type_id, $type_ids)) {
+                $msg = get_string('filter_no_type_mapping_error', 'filter_mediacore');
+                return $this->_get_embed_error_html($msg);
             }
-            // use the slug to query the MediaCore API to get the embed code (with signed lti params if applicable)
-            $embed_html = $this->_mcore_media->fetch_media_embed($slug, $COURSE->id, $type_id);
-            if ($embed_html) {
-                return $embed_html;
-            }
+        }
+        // use the slug to query the MediaCore API to get the embed code (with signed lti
+        // params if applicable)
+        $embed_html = $this->_mcore_media->fetch_media_embed($slug, $COURSE->id, $type_id);
+        if ($embed_html) {
+            return $embed_html;
         }
         return $this->_get_embed_error_html($msg);
     }
