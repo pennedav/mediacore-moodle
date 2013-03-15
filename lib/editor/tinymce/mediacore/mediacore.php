7eb66035946cd1aa58acb2817d8a31f0a2948a84
Derek Harnanansingh <derek@mediacore.com>
2013-03-15 00:47:51 -0700

    Update the TinyMCE plugin
    * Simplify the course_id/type_id page logic

--- lib/editor/tinymce/mediacore/mediacore.php
+++ lib/editor/tinymce/mediacore/mediacore.php
@@ -19,21 +19,31 @@ include_once(dirname(__FILE__) . '/../../../../../../../config.php');
 defined('MOODLE_INTERNAL') || die('Can\'t find path/to/moodle/config.php.');
 require_once($CFG->dirroot . '/local/mediacore/lib.php');
 
-//defaults
-$lti_tools = array(); $media = null;
-$course_id = (isset($_GET['course_id'])) ? (int)$_GET['course_id'] : 0;
-$type_id = (isset($_GET['type_id']) && $_GET['type_id'] > -1) ? (int)$_GET['type_id'] : 0;
-$is_public_type_id = (isset($_GET['type_id']) && $_GET['type_id'] == 0);
-$lti_tools = local_mediacore_fetch_lti_tools_by_course_id($course_id);
+$lti_baseurl = local_mediacore_fetch_lti_baseurl();
+$mcore_media = new mediacore_media($lti_baseurl);
 
-//display logic
-if (isset($course_id, $type_id) || $is_public_type_id) {
-	$mediacore_url = local_mediacore_fetch_lti_url();
-	$mcore_media = new mediacore_media($mediacore_url);
-	$curr_page = (isset($_GET['page'])) ? (int)$_GET['page'] : 0;
-	$search = (isset($_GET['search'])) ? (string)$_GET['search'] : '';
-	$media = $mcore_media->fetch_media($curr_page, $search, $limit = 6, $course_id, $type_id);
+//api params
+$curr_page = (isset($_GET['page'])) ? (int)$_GET['page'] : 0;
+$search = (isset($_GET['search'])) ? (string)$_GET['search'] : '';
+
+//course id
+$lti_tools = NULL; $lti_tool_ids = NULL;
+$course_id = (isset($_GET['course_id'])) ? (int)$_GET['course_id'] : NULL;
+$course_id_val = ($course_id) ? $course_id : '';
+if (!empty($course_id)) {
+	$lti_tools = local_mediacore_fetch_lti_tools_by_course_id($course_id);
+	$lti_tool_ids = local_mediacore_fetch_lti_tool_ids_by_course_id($course_id);
 }
+
+//type id
+$type_id = (isset($lti_tool_ids[0])) ? (int)$lti_tool_ids[0] : NULL;
+if (!empty($_GET['type_id'])) {
+	$type_id = (int)$_GET['type_id'];
+}
+$type_id_val = ($type_id) ? $type_id : '';
+
+//media
+$media = $mcore_media->fetch_media($curr_page, $search, $limit=6, $course_id, $type_id);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html dir="ltr" lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
@@ -48,17 +58,17 @@ if (isset($course_id, $type_id) || $is_public_type_id) {
 		<div id="mcore-embed">
 			<div class="mcore-lti-select mcore-clearfix">
 				<form action="" method="get">
-					<label for="type_id">Connection type:</label>
+					<label for="type_id">Please select:</label>
 					<select name="type_id" onchange="this.form.submit();">
 					<?php echo local_mediacore_build_connection_options($lti_tools, $type_id); ?>
 					</select>
-					<input name="course_id" type="hidden" value="<?php echo $course_id; ?>" />
+					<input name="course_id" type="hidden" value="<?php echo $course_id_val; ?>" />
 				</form>
 			</div>
 			<div class="mcore-header">
 				<div class="mcore-pagination">
 					<a <?php if ($mcore_media->has_previous_page()): ?>
-						href="?page=<?php echo $mcore_media->get_previous_page() . "&type_id=$type_id&course_id=$course_id"; ?>"
+						href="?page=<?php echo $mcore_media->get_previous_page() . "&type_id=$type_id_val&course_id=$course_id_val"; ?>"
 						<?php endif; ?>
 						class="mcore-btn mcore-prev <?php if (!$mcore_media->has_previous_page()) echo "mcore-disabled"; ?>"
 						title="Goto Previous Page"
@@ -66,7 +76,7 @@ if (isset($course_id, $type_id) || $is_public_type_id) {
 						&#8656;
 					</a>
 					<a <?php if ($mcore_media->has_next_page()): ?>
-						href="?page=<?php echo $mcore_media->get_next_page() . "&type_id=$type_id&course_id=$course_id"; ?>"
+						href="?page=<?php echo $mcore_media->get_next_page() . "&type_id=$type_id_val&course_id=$course_id_val"; ?>"
 						<?php endif; ?>
 						class="mcore-btn mcore-next <?php if(!$mcore_media->has_next_page()) echo "mcore-disabled"; ?>"
 						title="Goto Next Page"
@@ -79,15 +89,15 @@ if (isset($course_id, $type_id) || $is_public_type_id) {
 			<form class="mcore-search" action="" method="get">
 				<div>
 					<?php if ($mcore_media->get_search_query()): ?>
-					<a class="mcore-clear-search" href="?course_id=<?php echo $course_id; ?>&type_id=<?php echo $type_id; ?>&search=">x</a>
+					<a class="mcore-clear-search" href="?course_id=<?php echo $course_id_val; ?>&type_id=<?php echo $type_id_val; ?>&search=">x</a>
 					<?php endif; ?>
-					<input name="course_id" type="hidden" value="<?php echo $course_id; ?>" />
-					<input name="type_id" type="hidden" value="<?php echo $type_id; ?>" />
+					<input name="course_id" type="hidden" value="<?php echo $course_id_val; ?>" />
+					<input name="type_id" type="hidden" value="<?php echo $type_id_val; ?>" />
 					<input type="text" name="search" id="search" class="mcore-search-field" value="<?php echo $search; ?>" />
 				</div>
 			</form>
 			<div class="mcore-content">
-			<?php if ($media) : //public or lti connection selected ?>
+			<?php if ($media) : ?>
 				<?php if ($mcore_media->get_rowset_count()== 0): ?>
 				<div class="mcore-message">
 					<img src="images/zero-state.png" alt="zero state">
@@ -100,7 +110,7 @@ if (isset($course_id, $type_id) || $is_public_type_id) {
 				?>
 				<div class="mcore-media mcore-clearfix mcore-video">
 					<div class="mcore-thumbnail">
-						<a href="javascript:MediaCorePopup.insert('<?php echo $m->get_url(); ?>', '<?php echo $m->get_escaped_title(); ?>', '<?php echo $type_id; ?>');">
+						<a href="javascript:MediaCorePopup.insert('<?php echo $m->get_url(); ?>', '<?php echo $m->get_escaped_title(); ?>', '<?php echo $type_id_val; ?>');">
 							<img src="<?php echo $m->get_thumbs_small_url(); ?>"
 							alt="<?php echo $m->get_title(); ?>" />
 							<span class="mcore-border"></span>
@@ -115,7 +125,7 @@ if (isset($course_id, $type_id) || $is_public_type_id) {
 					</div>
 					<div class="mcore-info">
 						<h3>
-							<a class="ellipsis" href="javascript:MediaCorePopup.insert('<?php echo $m->get_url(); ?>', '<?php echo $m->get_escaped_title(); ?>', '<?php echo $type_id; ?>');">
+							<a class="ellipsis" href="javascript:MediaCorePopup.insert('<?php echo $m->get_url(); ?>', '<?php echo $m->get_escaped_title(); ?>', '<?php echo $type_id_val; ?>');">
 								<?php echo $m->get_title(); ?>
 							</a>
 						</h3>
@@ -126,21 +136,16 @@ if (isset($course_id, $type_id) || $is_public_type_id) {
 					<div class="mcore-add">
 						<span class="mcore-btn mcore-add-btn">
 							<!-- These links are ugly. -->
-							<a href="javascript:MediaCorePopup.insert('<?php echo $m->get_url(); ?>', '<?php echo $m->get_escaped_title(); ?>', '<?php echo $type_id; ?>');">
+							<a href="javascript:MediaCorePopup.insert('<?php echo $m->get_url(); ?>', '<?php echo $m->get_escaped_title(); ?>', '<?php echo $type_id_val; ?>');">
 								<span class="mcore-icon"></span>Add</a>
 						</span>
 					</div>
 				</div> <!-- /.mcore-media -->
 				<?php endforeach ?>
-			<?php else: // no LTI confi selected!  ?>
+			<?php else: // no media found  ?>
 			<div class="mcore-message">
-				<?php if ($type_id === null): ?>
-				<h2>Configure LTI</h2>
-				<p><?php echo get_string('tinymce_lti_desc', 'local_mediacore'); ?></p>
-				<?php else: ?>
 				<h2>No Media</h2>
 				<p><?php echo get_string('tinymce_no_media_found', 'local_mediacore'); ?></p>
-				<?php endif; ?>
 			<?php endif; ?>
 			</div> <!-- /.mcore-content -->
 			<div class="mcore-footer">
