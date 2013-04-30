c9481e9ece2eeff770f3cb407d5d21379a92548d
Derek Harnanansingh <derek@arietis-software.com>
2013-04-30 13:47:21 -0700

    Update the local/mediacore libs.
    * Update for LTI
    * Refactor classes
    * Delete locallib
    * Update settings page
    * Rename settings constant
    * Add new upgrade script to remove old config db data from v1.6

--- local/mediacore/lang/en/local_mediacore.php
+++ local/mediacore/lang/en/local_mediacore.php
@@ -15,18 +15,8 @@
  */
 
 //plugin settings
-$string['pluginname'] = 'MediaCore package libraries';
-$string['setting_url_heading_title'] = 'MediaCore URL:';
-$string['setting_url_label'] = 'Your MediaCore URL:';
-$string['setting_url_desc'] = 'Please enter your MediaCore URL in the textfield below.';
-$string['setting_courses_heading_title'] = 'MediaCore LTI-To-Course Mappings:';
-$string['setting_courses_heading_desc'] = 'Please select the LTI tool domains you wish to allow each course below to have access to. This will affect which LTI tool domains a course can embed media from.';
-$string['setting_course_fullname_prefix'] = '';
-$string['setting_course_lti_desc'] = 'Please select the LTI configurations above that you would like this course to have access to.';
+$string['pluginname'] = 'MediaCore package config';
+$string['setting_url_label'] = 'MediaCore URL:';
+$string['setting_consumer_key_label'] = 'MediaCore Consumer Key';
+$string['setting_shared_secret_label'] = 'MediaCore Shared Secret';
 
-//tinymce popup connection select list
-$string['tinymce_select_default'] = 'Please select...';
-$string['tinymce_public_connection'] = 'Public media';
-$string['tinymce_lti_desc'] = 'Please select an LTI configuration from the drop down above to continue or select "public" for public media.';
-$string['tinymce_connection_error'] = 'There was an error connecting to your MediaCore site. Please verify your config settings and reload this page to continue.';
-$string['tinymce_no_media_found'] = 'No media found';
--- local/mediacore/settings.php
+++ local/mediacore/settings.php
@@ -13,43 +13,33 @@
  * @copyright  2012 MediaCore Technologies
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+require_once($CFG->dirroot. '/local/mediacore/lib.php');
+
 defined('MOODLE_INTERNAL') || die('Invalid access');
 
 if ($hassiteconfig) {
 
-    require_once($CFG->dirroot . '/local/mediacore/locallib.php');
-
-    $settings = new admin_settingpage('local_mediacore', get_string('pluginname', 'local_mediacore'));
+    $settings = new admin_settingpage(MEDIACORE_PLUGIN_NAME, get_string('pluginname',
+            MEDIACORE_PLUGIN_NAME));
     $ADMIN->add('localplugins', $settings);
 
-    $settings->add(new admin_setting_heading('setting_url_heading',
-        get_string('setting_url_heading_title', 'local_mediacore'),
-        '', 'local_mediacore'));
+    $setting = new admin_setting_configtext(MEDIACORE_SETTINGS_NAME . '/url',
+            get_string('setting_url_label', MEDIACORE_PLUGIN_NAME),
+            '', 'http://demo.mediacore.tv', PARAM_TEXT);
+    $setting->plugin = MEDIACORE_SETTINGS_NAME;
+    $settings->add($setting);
 
-    $admin_setting = new admin_setting_configtext('local_mediacore_courselti/mediacore_url',
-        get_string('setting_url_label', 'local_mediacore'),
-        get_string('setting_url_desc', 'local_mediacore'),
-        'http://demo.mediacore.tv', PARAM_TEXT);
-    $admin_setting->plugin = MEDIACORE_LOCAL_COURSELTI_SETTING_NAME;
-    $settings->add($admin_setting);
+    $setting = new admin_setting_configtext(MEDIACORE_SETTINGS_NAME .'/consumer_key',
+            get_string('setting_consumer_key_label', MEDIACORE_PLUGIN_NAME),
+            '', '', PARAM_TEXT);
+    $setting->plugin = MEDIACORE_SETTINGS_NAME;
+    $settings->add($setting);
 
-    $courses = local_mediacore_fetch_courses();
-    $lti_tools = local_mediacore_fetch_lti_tools();
-    if (!empty($courses) && !empty($lti_tools)) {
-        $lti_tool_choices = array();
-        foreach ($lti_tools as $t) {
-            $lti_tool_choices[$t->id] = $t->name;
-        }
-        $settings->add(new admin_setting_heading('setting_courses_heading',
-            get_string('setting_courses_heading_title', 'local_mediacore'),
-            '', 'local_mediacore'));
-        foreach ($courses as $c) {
-            $header_str = get_string('setting_course_fullname_prefix', 'local_mediacore');
-            $admin_setting = new admin_setting_configmulticheckbox('local_mediacore_courselti/' . $c->id,
-                "<strong>$c->fullname</strong>",
-                get_string('setting_course_lti_desc', 'local_mediacore'), NULL, $lti_tool_choices);
-            $admin_setting->plugin = MEDIACORE_LOCAL_COURSELTI_SETTING_NAME;
-            $settings->add($admin_setting);
-        }
-    }
+    $setting = new admin_setting_configtext(MEDIACORE_SETTINGS_NAME . '/shared_secret',
+            get_string('setting_shared_secret_label', MEDIACORE_PLUGIN_NAME),
+            '', '', PARAM_TEXT);
+    $setting->plugin = MEDIACORE_SETTINGS_NAME;
+    $settings->add($setting);
+
 }
