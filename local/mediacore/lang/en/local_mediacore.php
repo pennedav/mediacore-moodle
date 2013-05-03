b483cbf4a7125b6b7d8e3ccb525e2bcf2e9bce08
Derek Harnanansingh <derek@mediacore.com>
2013-05-03 10:51:55 -0700

    Add field descriptors and help to the plugin settings

--- local/mediacore/lang/en/local_mediacore.php
+++ local/mediacore/lang/en/local_mediacore.php
@@ -16,7 +16,12 @@
 
 //plugin settings
 $string['pluginname'] = 'MediaCore package config';
-$string['setting_url_label'] = 'MediaCore URL:';
-$string['setting_consumer_key_label'] = 'MediaCore Consumer Key';
-$string['setting_shared_secret_label'] = 'MediaCore Shared Secret';
 
+$string['setting_url_label'] = 'MediaCore URL:';
+$string['setting_url_desc'] = '*If your Moodle site is being served over "https", please make sure your MediaCore URL above begins with "https" as well.';
+
+$string['setting_consumer_key_label'] = 'MediaCore Consumer Key';
+$string['setting_consumer_key_desc'] = '*This must match an existing <a href="http://support.mediacore.com/customer/portal/articles/869178-what-is-lti-integration-and-how-do-i-set-it-up-" target="_blank">LTI consumer key</a> in your MediaCore site above.';
+
+$string['setting_shared_secret_label'] = 'MediaCore Shared Secret';
+$string['setting_shared_secret_desc'] = '*This must match the shared secret from the LTI consumer key in your MediaCore site above.';
